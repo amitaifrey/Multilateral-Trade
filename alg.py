41be215649eaeec92f495eeb656f0d8026a5e363
Amitai Frey <amifre@gmail.com>
2023-07-23 20:29:08 +0300

    Fixed get funcs

--- alg.py
+++ alg.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
 
-SAMPLE_SIZE = 2000
+SAMPLE_SIZE = 1000
 MIN_PROB = 1/SAMPLE_SIZE
 
 def init_buyer_arrays(S, P_b):
@@ -29,7 +29,7 @@ def get_best_price(S, C_s, C_b, E_s, E_b):
     return max_price
 
 def init_matrix(S, P_1, P_2, C_s, E_s):
-    G = np.ndarray((len(S), len(S)))
+    G = np.zeros((len(S), len(S)))
     G[-1, -1] = P_1[-1] * P_2[-1] * (S[-1] - E_s[-1]) * C_s[-1]
     for i in range(len(S)-2, 0, -1):
         G[i, -1] = P_1[-1] * P_2[i] * (S[-1] - E_s[-1]) * C_s[-1] + G[i+1, -1]
@@ -59,12 +59,24 @@ def find_funcs(G, P_1, P_2, C_s, C_1, C_2, E_s, E_1, E_2, S, p_1, p_2):
         t = max(S[i], S[j])
         if i == len(S) - 1:
             j = j + 1
-        elif j == len(S) - 1:
+            continue
+        if j == len(S) - 1:
             i = i + 1
-        elif G[i, j] == P_1[j] * (P_2[i] * t + C_2[i] * E_2[i] - C_2[i - 1] * E_s[i]) * C_s[i] + G[i, j + 1]:
+            continue
+        i_valid = np.isclose(G[i, j], P_2[i] * (P_1[j] * t + C_1[j] * E_1[j] - C_1[j - 1] * E_s[j]) * C_s[j] + G[i + 1, j])
+        j_valid = np.isclose(G[i, j], P_1[j] * (P_2[i] * t + C_2[i] * E_2[i] - C_2[i - 1] * E_s[i]) * C_s[i] + G[i, j + 1])
+        if not i_valid or not j_valid:
+            print(S[i], S[j], i_valid, j_valid)
+            print(P_1[j] * (P_2[i] * t + C_2[i] * E_2[i] - C_2[i - 1] * E_s[i]) * C_s[i] + G[i, j + 1] - P_2[i] * (P_1[j] * t + C_1[j] * E_1[j] - C_1[j - 1] * E_s[j]) * C_s[j] + G[i + 1, j])
+        if i_valid and j_valid:
+            if i >= j:
+                j = j + 1
+            else:
+                i = i + 1
+        elif j_valid:
             j = j + 1
         else:
-            i = i +1
+            i = i + 1
     return f_1, f_2
 
 def run(S, P_s, P_1, P_2):
@@ -84,14 +96,18 @@ def run(S, P_s, P_1, P_2):
     f_1, f_2 = find_funcs(G, P_1, P_2, C_s, C_1, C_2, E_s, E_1, E_2, S, p_1, p_2)
     t3 = time.time()
     print("find funcs:", t3 - t2)
-    return f_1, f_2
+    return f_1, f_2, G
 
 def draw_funcs(f_1, f_2):
     plt.plot(f_1.values(), f_1.keys(), color='blue', drawstyle='steps-post')
     plt.plot(f_2.keys(), f_2.values(), color='blue', drawstyle='steps-post')
-    #plt.hlines(y=0.25, xmin = 0, xmax = 0.5, color='red')
+    # plt.hlines(y=0.25, xmin = 0, xmax = 0.5, color='red')
     # plt.vlines(x=0.5, ymin=0, ymax=0.25, color='red')
     # plt.plot((0.5,0.625), (0.25,0.5), color='red')
+    plt.show()
+
+def draw_matrix(G):
+    plt.imshow(G, cmap='hot', interpolation='nearest')
     plt.show()
 
 def fixed_prices(v_s, v_1, v_2, p1, p2):
@@ -138,29 +154,36 @@ def sample_dists():
     return S, P_s, P_1, P_2
 
 def create_dists_grid():
-    S_s  = np.arange(0, 1, 1/SAMPLE_SIZE)
-    S_1 = np.arange(0, 1, 1 / SAMPLE_SIZE)
-    #S_2 = np.arange(0, 0.5, 1 / (2 * SAMPLE_SIZE))
-    S_2 = np.concatenate((np.arange(0, 0.1, 1 / (2*SAMPLE_SIZE)), np.arange(0.3, 0.4, 1 / (2*SAMPLE_SIZE)), np.arange(0.5, 0.6, 1 / (2*SAMPLE_SIZE)), np.arange(0.7, 0.8, 1 / (2*SAMPLE_SIZE)), np.arange(0.9, 1.0, 1 / (2*SAMPLE_SIZE))))
-    print(S_2)
+    S_s = np.array([0.5]*SAMPLE_SIZE)
+    S_1 = np.logspace(-1, 0.0, SAMPLE_SIZE)
+    S_2 = 1.0 - np.logspace(-1, 0.0, SAMPLE_SIZE)
+    #step_size = 0.2 /  SAMPLE_SIZE
+    #S_2 = np.concatenate((np.arange(0, 0.1, step_size), np.arange(0.9, 1.0, step_size)))
     S = np.sort(np.unique(np.concatenate((S_s, S_1, S_2, [0, 1]))))
     P_s = np.where(np.isin(S, S_s), 1 / SAMPLE_SIZE, 0)
     P_1 = np.where(np.isin(S, S_1), 1 / SAMPLE_SIZE, 0)
-    P_2 = np.where(np.isin(S, S_2), 1 / SAMPLE_SIZE, 1e-8)
+    P_2 = np.where(np.isin(S, S_2), 1 / SAMPLE_SIZE, 0)
     return S, P_s, P_1, P_2
 
 def create_dists_disc():
-    S_s = np.array([0])
-    S_1 = np.array([0.2, 0.4, 0.6, 0.8, 1.0])
-    S_2 = np.array([0.1, 0.3, 0.5, 0.7, 0.9])
-    S = np.sort(np.unique(np.concatenate((S_s, S_1, S_2))))
-    P_s = np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-    P_1 = np.array([0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0])
-    P_2 = np.array([0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0])
+    S_s = {0.0: 1.0}
+    S_1 = {0.1: 0.1, 0.3: 0.1, 0.5: 0.1, 0.9: 0.1}
+    S_1 = S_1 | {k: 0.6 / SAMPLE_SIZE for k in np.arange(0.6, 0.8, 0.2 / SAMPLE_SIZE)}
+    S_2 = {0.1: 0.1, 0.3: 0.1, 0.5: 0.1, 0.9: 0.1}
+    S_2 = S_2 | {k: 0.6 / SAMPLE_SIZE for k in np.arange(0.6, 0.8, 0.2 / SAMPLE_SIZE)}
+    print(np.sum(list(S_1.values())), np.sum(list(S_2.values())))
+    S = np.sort(np.unique(np.concatenate((list(S_s.keys()), list(S_1.keys()), list(S_2.keys()), [0, 1]))))
+    P_s = [S_s[k] if k in S_s else 0.0 for k in S]
+    P_1 = [S_1[k] if k in S_1 else 0.0 for k in S]
+    P_2 = [S_2[k] if k in S_2 else 0.0 for k in S]
     return S, P_s, P_1, P_2
 
 if __name__ == "__main__":
     S, P_s, P_1, P_2 = create_dists_disc()
-    f_1, f_2 = run(S, P_s, P_1, P_2)
+    f_1, f_2, G = run(S, P_s, P_1, P_2)
+    print(G[-10:,-10:])
+    print(f_1, f_2)
+    draw_matrix(G)
     draw_funcs(f_1, f_2)
+    #print(f_1, f_2)
     #calc_gft(f_1, f_2, S)
